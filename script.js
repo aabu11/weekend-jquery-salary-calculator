084909f85bdf0fd47e2532665cad85f7c35e7432
Ahmed Abu <ahmedabu13@gmail.com>
2022-12-04 13:16:43 -0600

    finished base mode

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
+let employeeList = [];
+let monthlyCost = 0;
 
+$(document).ready(onReady);
 
-let employeeList=[];
-
-$(document).ready(onReady) ;
+function onReady() {
+  $("#buttonToAddEmployee").on("click", addNewEmployee);
+  $("#buttonToAddEmployee").on("click", employeeSalary);
+  $("body").on("click", ".deleteemployee", deletebutton);
+  $('#buttonToAddEmployee').on('click', render)
  
-
-function onReady (){
- $('#buttonToAddEmployee').on('click',addNewEmployee);
-
 }
 
+function render() {
+   
+    if (monthlyCost > 20000) {
+        console.log(monthlyCost)
+      $("h3").css("background-color", "red");
+    }
+  }
 
- function addNewEmployee () {
+function addNewEmployee() {
+  let newFirstName = $("#employeefirstname").val();
+  let newLastName = $("#employeelastname").val();
+  let newJobTitle = $("#JobTitle").val();
+  let newID = $("#JobID").val();
+  let newAnnualSalary = $("#AnnualSalary").val();
 
-   let newFirstName = $('#employee first name').val();
-    let newLastName = $('#employee last name').val();
-    let newJobTitle = $('#Job Title').val();
-    let newID= $('#Job ID').val();
-    let newAnnualSalary=$('#Annual Salary').val();
-    
-  
-     let newEmployeeList= {
-         firstName:(newFirstName),
-        lastName:(newLastName),
-        ID:(newID),
-        jobTitle:(newJobTitle),
+  let newEmployeeList = {
+    firstName: newFirstName,
+    lastName: newLastName,
+    ID: newID,
+    jobTitle: newJobTitle,
+    annualSalary: newAnnualSalary,
+  };
 
-         annualSalary:(newAnnualSalary)
- }
+  employeeList.push(newEmployeeList);
 
-employeeList.push(newEmployeeList)
+  $(".rowData").empty();
 
-}
-
-function renderEmployees (){
-    $('#employeeList').empty();
-
-for (let i=0; i<employeeList.length; i++){
-
-    $('table').append (`
+  for (let i = 0; i < employeeList.length; i++) {
+    $("table").append(`
     <tr class="rowData">
             <th> ${employeeList[i].firstName}</th> 
             <th> ${employeeList[i].lastName}</th> 
+            <th> ${employeeList[i].ID}</th> 
             <th> ${employeeList[i].jobTitle}</th> 
             <th> ${employeeList[i].annualSalary}</th> 
-            <th> ${employeeList[i].lastName}</th> 
-
-    `)
+            <th><button class='deleteemployee'> Delete </button></th> 
+    </tr> `);
+  }
 }
+
+function deletebutton() {
+  let getRidOf = $(this).parent().parent();
+console.log(employeeList);
+  getRidOf.remove();
+}
+
+$("#employeefirstname").val("");
+$("#employeelastname").val("");
+$("#JobTitle").val("");
+$("#JobID").val("");
+$("#AnnualSalary").val("");
+render();
+
+function employeeSalary() {
+  let yearlySalary = $("#AnnualSalary").val();
+  let monthlySalary = yearlySalary / 12;
+  monthlyCost = monthlyCost + monthlySalary;
+  $("#Salary").text(monthlyCost);
+}
+
+function whenEmployeeDeleted (){
+    $('')
 
 }
